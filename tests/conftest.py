4de58b7c1527f16c9950dad83e90991046b0ccd0
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-02 00:52:57 -0400

    Refactors conftest to hold schemas + fix to unit schema test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,19 @@ def fixture__context_schema():
     path = "../schema/context.json"
     schema_file = this_file.parents[0] / pathlib.Path(path)
     return json.loads(schema_file.read_text())
+
+
+@pytest.fixture(name="unit_schema")
+def fixture__unit_schema():
+    this_file = pathlib.Path(__file__)
+    unit_path = "../schema/unit.json"
+    schema_file = this_file.parents[0] / pathlib.Path(unit_path)
+    return json.loads(schema_file.read_text())
+
+
+@pytest.fixture(name="value_schema")
+def fixture__value_schema():
+    this_file = pathlib.Path(__file__)
+    value_path = "../schema/value.json"
+    schema_file = this_file.parents[0] / pathlib.Path(value_path)
+    return json.loads(schema_file.read_text())
--- tests/schema/test_unit.py
+++ tests/schema/test_unit.py
@@ -5,62 +5,17 @@ import pytest
 
 from tests.utils import validate_with_schema_deps
 
-@pytest.fixture(name="unit_schema")
-def fixture__unit_schema():
-    this_file = pathlib.Path(__file__)
-    unit_path = "../../schema/unit.json"
-    schema_file = this_file.parents[0] / pathlib.Path(unit_path)
-    return json.loads(schema_file.read_text())
 
-def test_unit_schema(context_schema, unit_schema) -> None:
-    data = """
-    {
-        "@context": [
-            "https://stuchalk.github.io/scidata/contexts/scidata_unit.jsonld",
-            {"@base": "https://stuchalk.github.io/scidata/examples/unit.jsonld#"}
-        ],
-        "@id": "",
-        "name": "Mile per hour",
-        "unitsystemref": "sci:imperial",
-        "quantity": "speed",
-        "abbrev": "mph",
-        "symbol": "mile hr-1",
-        "unitinsi": {
-            "@id": "unitinsi",
-            "siunit": [
-                {
-                    "@id": "siunit/1",
-                    "name": "meter",
-                    "prefix": "1",
-                    "power": 1
-                },
-                {
-                    "@id": "siunit/2",
-                    "name": "second",
-                    "prefix": "1",
-                    "power": -1
-                }
-            ],
-            "factor": {
-                "@id": "mph/to/mps/conversionFactor",
-                "fromunit": "mile per hour",
-                "tounit": "meter per second",
-                "multiplier": 1609.34,
-                "divisor": 3600,
-                "exact": false
-            }
-        },
-        "factor": {
-            "@id": "mph/to/kph/conversionFactor",
-            "fromunit": "mile per hour",
-            "tounit": "kilometer per hour",
-            "multiplier": 1.60934,
-            "exact": false
-        }
-    }
-    """
-    data = json.loads(data)
-    assert validate_with_schema_deps(context_schema, unit_schema, data)
+@pytest.fixture(name="unit_jsonld")
+def fixture__unit_jsonld():
+    this_file = pathlib.Path(__file__)
+    path = "../../examples/sections/unit.jsonld"
+    data_file = this_file.parents[0] / pathlib.Path(path)
+    return json.loads(data_file.read_text())
+
+
+def test_unit_schema(context_schema, unit_schema, unit_jsonld) -> None:
+    assert validate_with_schema_deps(context_schema, unit_schema, unit_jsonld)
 
 
 def test_unit_schema_bad_name(context_schema, unit_schema) -> None:
